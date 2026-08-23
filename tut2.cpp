0d13835c485c53da87b110c18d608ccc09adafcf
NITIN KUMAR SHUKLA <nitinkumarco234@gmail.com>
2026-08-23 19:54:47 +0530

    Add files via upload

---
+++ tut2.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+
+using namespace std;
+int main(){
+    int a= 5;
+    int b= 6;
+    float pi = 3.14;
+    cout<<"a is equal to "<<a<<endl;
+    
+    cout<<"b is equal to "<<b<<endl;
+    cout<<"pi is equal to "<<pi<<endl;
+    return 0;
+}
