609f5435194b2c06ce69351cebade02c739478b1
NITIN KUMAR SHUKLA <nitinkumarco234@gmail.com>
2026-08-26 21:39:30 +0530

    Add files via upload

---
+++ tut4.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+int c=45;
+int main(){
+    // ****Built In Data types****
+    //int a, b, c;
+    // cout << "Enter the Value of a;"<<endl;
+    // cin>>a;
+    // cout << "Enter the Value of b;"<<endl;
+    // cin>>b;
+    // c=a+b;
+    // cout<< "the sum is" << c <<endl;
+    // cout<<"the global c is " <<::c;
+    // return 0;
+    // ****float, double and long double lieterals****
+    // float d=34.4F;
+    // long double e=34.4L;
+    // cout<<"The size of 34.4 is "<<sizeof(34.4)<<endl;
+    // cout<<"The size of 34.4f is "<<sizeof(34.4f)<<endl;
+    // cout<<"The size of 34.4F is "<<sizeof(34.4F)<<endl;
+    // cout<<"The size of 34.4l is "<<sizeof(34.4l)<<endl;
+    // cout<<"The size of 34.4L is "<<sizeof(34.4L)<<endl;
+    // cout<<"the value of d is"<<d<<endl<<"the value of e is"<<e;
+    //*******Reference Varaibles*****
+
+    // rohan das-----> monty ------> rohun-------> dangerous coder
+    // float x =455;
+    // float & y= x;
+    // cout <<x<<endl;
+    // cout<<y<<endl;    
+    // return 0;
+ 
+    //******typecasting*****
+    int a=45;
+    float b=45.46;
+    cout<<"the value of a is "<<(float)a; 
+    cout<<"the value of a is "<<float(a); 
+
+    cout<<"the value of b is "<<(int)b;
+    cout<<"the value of b is "<<int(b);
+    return 0;
+}
