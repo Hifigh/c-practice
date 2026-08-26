e9646621a85e6ba401bda32af00ea5c5a510de1a
NITIN KUMAR SHUKLA <nitinkumarco234@gmail.com>
2026-08-26 20:14:36 +0530

    Add files via upload

---
+++ tut3.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "enter the value of num1 :\n"; /* '<<' is called insertion operator */ ;
+    cin>>num1;
+
+    cout << "enter the valur of num2 :\n"; 
+    cin>> num2;
+
+    cout << "the sum is" << num1 + num2;
+    return 0;
+}
