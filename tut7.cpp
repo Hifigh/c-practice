36be59f70bb9b33b438c22ab72ee12d3edb3f40c
NITIN KUMAR SHUKLA <nitinkumarco234@gmail.com>
2026-09-01 22:38:56 +0530

    Add files via upload

---
+++ tut7.cpp
@@ -0,0 +1,28 @@
+#include <Iostream>
+using namespace std;
+
+int main(){
+    //loops in c++
+    //ist is for loop
+    
+    // for(int i=1;i<40;i++){
+    //     cout<<i<<endl;
+
+    // }
+
+    //2nd while loop
+    // int i=1;
+    // while(i<=40){
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    // 3rd do whilw loop
+    int i=1;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i<=40);
+    return 0;
+
+
+}
