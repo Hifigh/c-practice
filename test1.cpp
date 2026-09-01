eba70332fddd78d587e69b264f3b0604ecf0a7bb
NITIN KUMAR SHUKLA <nitinkumarco234@gmail.com>
2026-09-01 22:42:38 +0530

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// table of 6 using loop
+int main(){
+
+    for (int i = 0; i <= 10; i++)
+    {
+        cout<<(i*6)<<endl;/* code */
+    }
+    return 0;   
+}
